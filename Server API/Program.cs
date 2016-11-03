c080bb8bcf938fb8193d8bebd9d70e7d6dc904cb
BrennanButler <brennan9723@gmail.com>
2016-11-03 15:43:27 +0000

    Initial files

---
+++ Server API/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+// API KEY : AIzaSyAnQ3IBfEGJ7G7hAhhBlXUZDA48ikMHk7o
+
+namespace Server_API
+{
+    class Program
+    {
+        const string apiKey = "AIzaSyAnQ3IBfEGJ7G7hAhhBlXUZDA48ikMHk7o";
+
+        public static string ParseData(string data)
+        {
+
+            try
+            {
+                JObject origin = JObject.Parse(data);
+
+                MapData map = new MapData
+                {
+                    latitude = new List<double>
+                    {
+                        Double.Parse(origin["routes"][0]["legs"][0]["start_location"]["lat"].ToString()),
+                        Double.Parse(origin["routes"][0]["legs"][0]["end_location"]["lat"].ToString())
+                    },
+
+                    longitude = new List<double>
+                    {
+                        Double.Parse(origin["routes"][0]["legs"][0]["start_location"]["lng"].ToString()),
+                        Double.Parse(origin["routes"][0]["legs"][0]["end_location"]["lng"].ToString())
+                    },
+
+                    polyline = origin["routes"][0]["overview_polyline"]["points"].ToString(),
+
+                    totalDistance = Int32.Parse(origin["routes"][0]["legs"][0]["distance"]["value"].ToString())
+
+                };
+
+
+                return JsonConvert.SerializeObject(map, Formatting.Indented);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return "error";
+        }
+
+        static void Main(string[] args)
+        {
+
+            Server serverO = new Server(HttpResponse, "http://127.0.0.1:8080/");
+
+            serverO.StartListening();
+   
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        public static string HttpResponse(HttpListenerRequest request)
+        {
+
+            // Return the word error if an error occurs 
+            if (request.HttpMethod != "GET")
+                return "error";
+          
+            // Return the word error if an error occurs 
+            if (request.QueryString["origin"] == null || request.QueryString["origin"].Length == 0
+                || request.QueryString["destination"] == null || request.QueryString["destination"].Length == 0)
+                return "error";
+
+            // Construct a url that we can use to request data from google maps
+            string requestUrl = "https://maps.googleapis.com/maps/api/directions/json?";
+            requestUrl += "origin=" + request.QueryString["origin"] + "&destination=" + request.QueryString["destination"]
+                + "&key=" + apiKey;
+           
+            WebRequest googleMaps = WebRequest.Create(requestUrl);
+
+            googleMaps.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse response = googleMaps.GetResponse();
+            
+            // Read the response
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string mapsResponse = reader.ReadToEnd();
+            string parsedJson = ParseData(mapsResponse);
+            
+            return parsedJson;
+        }
+
+        
+    }
+}
---
+++ Server API/Server.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Server_API
+{
+
+    // Used to store the data we want from the API request
+    class MapData
+    {
+        public IList<double> latitude { get; set; }
+        public IList<double> longitude { get; set; }
+        public string polyline { get; set; }
+        public int totalDistance { get; set; }
+    }
+
+    
+
+    class Server
+    {
+ 
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> responseMethod;
+
+
+        public Server(string[] prefix, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("Your system OS is not supported");
+
+            if (prefix == null || prefix.Length == 0)
+                throw new ArgumentException("Prefixes cannot be null");
+
+            if (method == null)
+                throw new ArgumentException("Method cannot be null");
+
+            foreach (string s in prefix)
+                _listener.Prefixes.Add(s);
+
+            responseMethod = method;
+            
+            _listener.Start();
+
+        }
+
+        public Server(Func<HttpListenerRequest, string> method, params string[] prefix)
+            : this(prefix, method)
+        {}
+
+        public void StartListening()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Listening...");
+
+                while(_listener.IsListening)
+                {
+                    ThreadPool.QueueUserWorkItem((c) =>
+                    {
+                        var context = c as HttpListenerContext;
+                        try
+                        {
+                            string responseStr = responseMethod(context.Request);
+                            byte[] buffer = Encoding.UTF8.GetBytes(responseStr);
+                            context.Response.ContentLength64 = buffer.Length;
+                            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                        }
+                        catch { }
+                        finally
+                        {
+                            context.Response.Close();
+                        }
+
+                    }, _listener.GetContext());
+                }
+            });
+        }
+    }
+}
